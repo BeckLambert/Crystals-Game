afa45ec151bfdfec047a0c9462b0cb5244a94ef8
lambertco <blambertco@gmail.com>
2019-09-11 19:51:24 -0600

    random number for buttons functions completed and working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,23 +3,26 @@ var targetScore = "";
 var roundScore = "";
 var wins = "";
 var losses = "";
-// var crystals = {
-//     crystal1: {
-//         val: '',
-//         name: ''
-//     }
-// }
+var crystals = ["#assets/images/crystalOne.jpg",
+    "assets/images/crystalTwo.jpg",
+    "assets/images/crystalThree.jpg",
+    "assets/images/crystalFour.jpg"]
+console.log(crystals);
 
 // The player will be shown a random number at the start of the game between 19 - 120..
-     function startGame() {
-        targetScore = 19 + Math.floor(Math.random() * 121);
-        console.log("Target Score: " + targetScore);
-        $("#target-score").text("Target Score: " + targetScore);
-     };
+function startGame() {
+    targetScore = (19 + Math.floor(Math.random() * 121));
+    $("#target-score").text(targetScore);
+    console.log(targetScore);
+}
+startGame()
 
 // When the player clicks on a crystal, it will add a specific amount of points to the player's total score with a random hidden value between 1 - 12.
-$(".crystals").on("click", function() {
-    alert("clicked!");
+$(".crystals").on("click", function () {
+    for (var i = 0; i < crystals.length; i++)
+        var crystalValue = 1 + Math.floor(Math.random() * 11);
+    console.log(crystalValue);
+    $("button").val(crystalValue);
 });
 // When they do click one, update the player's score counter.
 //     function addToRoundScore() {}
@@ -39,5 +42,5 @@ $(".crystals").on("click", function() {
 
 
 
-   
-      
+
+
