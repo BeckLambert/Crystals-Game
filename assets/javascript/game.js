857bbad34dc8909654f69ed291870a5356096438
lambertco <blambertco@gmail.com>
2019-09-11 18:12:54 -0600

    psuedo coded out game

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+//   Global Variables
+var targetScore = "";
+var roundScore = "";
+var wins = "";
+var losses = "";
+var crystals = {
+    crystal1: {
+        val: '',
+        name: ''
+    }
+}
+ //     function startGame() {}
+
+// The player will be shown a random number at the start of the game between 19 - 120..
+ 
+  //     function generateRandomNumber() {
+    //       return Math...
+    //     }
+
+// When the player clicks on a crystal, it will add a specific amount of points to the player's total score with a random hidden value between 1 - 12.
+// When they do click one, update the player's score counter.
+//     function addToRoundScore() {}
+
+
+// The player wins if their total score matches the random number from the beginning of the game.
+// The player loses if their score goes above the random number.
+// The app should show the number of games the player wins and loses. 
+ //     function checkIfWon() {}
+
+
+
+// The game restarts whenever the player wins or loses.
+// When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
+//     function gameReset() {}
+
+
+
+
+    //   JQUERY CLICK EVENTS
+    //     ${'.crystals1').on('click', function() {})}
